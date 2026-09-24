cecf7d8a83aab7d5856a17ffa1246ea87454a5d3
Mouli2022 <2022wb86572@wilp.bits-pilani.ac.in>
2026-09-24 18:54:36 +0530

    Update output message in Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from 2022WB86572");
+        System.out.println("Continuous Integration is Working");
     }
 }
